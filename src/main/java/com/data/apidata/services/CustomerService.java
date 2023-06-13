daf6863a3b45aad565c7b5b3235630af41ee1590
leonardosilva03 <131895356+leonardosilva03@users.noreply.github.com>
2023-06-13 10:46:36 -0300

    fix CustomerService.java

--- src/main/java/com/data/apidata/services/CustomerService.java
+++ src/main/java/com/data/apidata/services/CustomerService.java
@@ -2,20 +2,18 @@ package com.data.apidata.services;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import com.data.apidata.repository.*;
-import com.data.apidata.model.*;
-import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.data.apidata.model.Customer;
+import com.data.apidata.repository.CustomerRepository;
+
+import lombok.Getter;
 
 @Service
 public class CustomerService {
+
     private final CustomerRepository customerRepository;
 
     @Autowired
@@ -23,38 +21,7 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
-    public List<Customer> getAllCustomers() {
-        return customerRepository.findAll();
-    }
-
-    public Customer getCustomerByDocument(String document) {
+    public List<Customer> getCustomersByDocument(String document) {
         return customerRepository.findByDocument(document);
-    }
-
-    public Customer createCustomer(Customer customer) {
-        return customerRepository.save(customer);
-    }
-
-    public Customer updateCustomerByDocument(String document, Customer updatedCustomer) {
-        Customer existingCustomer = customerRepository.findByDocument(document);
-        if (existingCustomer != null) {
-            existingCustomer.setName(updatedCustomer.getName());
-            existingCustomer.setEmail(updatedCustomer.getEmail());
-            // Atualizar outros campos necessários
-
-            return customerRepository.save(existingCustomer);
-        } else {
-            return null;
-        }
-    }
-
-    public boolean deleteCustomerByDocument(String document) {
-        Customer existingCustomer = customerRepository.findByDocument(document);
-        if (existingCustomer != null) {
-            customerRepository.delete(existingCustomer);
-            return true;
-        } else {
-            return false;
-        }
     }
 }
